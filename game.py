d554acb37eae36ecd9f19dedfc33f414228b5be4
AaronL87 <49658070+AaronL87@users.noreply.github.com>
2020-01-26 17:49:44 -0500

    Update game.py

--- game.py
+++ game.py
@@ -2,9 +2,9 @@ import numpy as np
 import random
 
 # Currently, the robber moves first,
-# meaning that the robber can lose by moving onto the cop.
+# meaning that the robber, if 'random,' can lose by moving onto the cop.
 # This avoids the players not acknowledging when they cross paths,
-# Though there are other ways to avoid this that are less 'turn-based.'
+# though there are other ways to avoid this that are less 'turn-based.'
 
 class createGame:
     def __init__(self):
@@ -31,11 +31,10 @@ def placeRob(game,place):
         game.matrix[m_pr][n_pr] = game.robName
         return [m_pr,n_pr]
 
-def robChar(game,howDrunk,escape,twoMoveProb):
+def robChar(game,howDrunk,escape,rob2Move):
     game.robDrunk = howDrunk # Doesn't move this percent of the time
     game.robEscape = escape # If true, it tries to avoid cop
-    game.robTwoMove = twoMoveProb # Moves two squares this percent of the time
-    return game
+    game.rob2Move = rob2Move # Moves two squares this percent of the time
 
 def placeCop(game,place): 
     # place is a location on a 0-indexed, mxn matrix that is not occupied by the robber
@@ -57,29 +56,28 @@ def placeCop(game,place):
 def copChar(game,howDrunk,chase):
     game.copDrunk = howDrunk # Doesn't move this percent of the time
     game.copChase = chase # If true, it tries to catch robber
-    return game
 
 def startChase(game):
     go = drunkFunc(game,'rob')
     if go == True:
         if game.robEscape == False: # Robber randomly moves
-            game = randomMove(game,'rob')
+            randomMove(game,'rob')
         else: # Robber avoids cop
-            game = directFunc(game,'rob')
-        caught,game = checkIfCaught(game)
+            directFunc(game,'rob')
+        caught = checkIfCaught(game)
         if caught == True:
-            return caught,game
+            return caught
     
     go = drunkFunc(game,'cop')
     if go == True:  
         if game.copChase == True: # Cop chases robber
-            game = directFunc(game,'cop')
+            directFunc(game,'cop')
         else: # Cop randomly moves
-            game = randomMove(game,'cop')
-        caught,game = checkIfCaught(game)
-        return caught,game
+            randomMove(game,'cop')
+        caught = checkIfCaught(game)
+        return caught
     else:
-        return False, game
+        return False
 
 def drunkFunc(game,playerString):
     print('Move:',playerString)
@@ -102,71 +100,78 @@ def sidePick(game,playerString):
 
 def randomMove(game,playerString): #Finds possible move for player
     place,name,drunk = sidePick(game,playerString)
-
     game.matrix[place[0]][place[1]] = 0 # Removes previous location
-    oldVert = place[0] # Stores previous location to check if player moved vertically
-    game = vertMove(game,place,name,[-1,0,1])
-    if oldVert == place[0]: # Must move horizontally if not vertically
-        game = horizMove(game,place,name,[-1,1])
-    else:
-        game = horizMove(game,place,name,[-1,0,1])
-    return game
+    
+    if playerString == 'rob':
+        if game.rob2Move == False:
+            move(game,game.robPlace,game.robName,game.oneMoveList)
+        elif game.rob2Move == True:
+            move(game,game.robPlace,game.robName,game.twoMoveList)
+    elif playerString == 'cop':
+        move(game,game.copPlace,game.copName,game.oneMoveList)
 
-def vertMove(game,place,name,choices):
-    vert = random.choice(choices)
-    place[0] += vert
-    place[0] %= game.m
-    print('Up/Down:',vert,'to',place[0])
-    return game
+def move(game,place,name,choices):
+    moveTup = random.choice(choices)
+    
+    place[0] = (place[0] + moveTup[0]) % game.m
+    print('Up/Down:',moveTup[0],'to',place[0]) 
+    
+    place[1] = (place[1] + moveTup[1]) % game.n  
+    print('Side/Side:',moveTup[1],'to',place[1],'\n')
+    
+    game.matrix[place[0]][place[1]] = name 
 
-def horizMove(game,place,name,choices):
-    horiz = random.choice(choices)
-    place[1] += horiz 
-    place[1] %= game.n   
-    game.matrix[place[0]][place[1]] = name
-    print('Side/Side:',horiz,'to',place[1],'\n')
-    return game
-
-def directFunc(game,playerString): # Has cop chase robber
+def directFunc(game,playerString): # Cop chase robber and/or robber avoids cop.
     place,name,drunk = sidePick(game,playerString)
     robPlace = game.robPlace
     copPlace = game.copPlace
     
-    game.matrix[place[0]][place[1]] = 0 # Removes cop's previous location
+    game.matrix[place[0]][place[1]] = 0 # Removes players' previous location
     #vertical
     if (robPlace[0]-copPlace[0])%game.m > (copPlace[0]-robPlace[0])%game.m:
-        place[0] = (place[0]-1)%game.m # Cop moves up
+        place[0] = (place[0]-1)%game.m # Player moves up
     elif (robPlace[0]-copPlace[0])%game.m < (copPlace[0]-robPlace[0])%game.m:
-        place[0] = (place[0]+1)%game.m # Cop moves down
+        place[0] = (place[0]+1)%game.m # Player moves down
     elif (robPlace[0]-copPlace[0])%game.m == 0: # Catches where cop and robber are on same row
-        pass
+        if playerString == 'rob':
+            place[0] = (place[0]-1)%game.m # Robber moves up, but cop stays
     elif (robPlace[0]-copPlace[0])%game.m == (copPlace[0]-robPlace[0])%game.m:
-        place[0] = (place[0]-1)%game.m # Breaks stalemate by choosing up
+        if playerString == 'cop':
+            place[0] = (place[0]-1)%game.m # Cop moves up, but robber stays
     #horizontal
     if (robPlace[1]-copPlace[1])%game.n > (copPlace[1]-robPlace[1])%game.n:
-        game.matrix[place[0]][(place[1]-1)%game.n] = name # Cop moves left
         place[1] = (place[1]-1)%game.n
+        game.matrix[place[0]][place[1]] = name # Player moves left
     elif (robPlace[1]-copPlace[1])%game.n < (copPlace[1]-robPlace[1])%game.n:
-        game.matrix[place[0]][(place[1]+1)%game.n] = name # Cop moves right
         place[1] = (place[1]+1)%game.n
-    elif (robPlace[1]-copPlace[1])%game.n == 0: # Catches where cop and robber are on same row
-        game.matrix[place[0]][place[1]] = name # Horizontal stay same
+        game.matrix[place[0]][place[1]] = name # Player moves right
+    elif (robPlace[1]-copPlace[1])%game.n == 0: # Catches where cop and robber are on same column
+        if playerString == 'rob':
+            place[1] = (place[1]-1)%game.m # Robber moves left, but cop stays
+        game.matrix[place[0]][place[1]] = name
     elif (robPlace[1]-copPlace[1])%game.n == (copPlace[1]-robPlace[1])%game.n:
-        game.matrix[place[0]][(place[1]-1)%game.n] = name # Breaks stalemate by choosing left
-        place[1] = (place[1]-1)%game.n
-    return game
+        if playerString == 'cop':
+            place[1] = (place[1]-1)%game.n
+        game.matrix[place[0]][place[1]] = name # Cop moves left, but robber stays
 
 def checkIfCaught(game):
     if game.robPlace != game.copPlace:
         game.matrix[game.copPlace[0]][game.copPlace[1]] = game.copName
-        return False,game
+        return False
     elif game.robPlace == game.copPlace:
         game.matrix[game.robPlace[0]][game.robPlace[1]] = 3
-        return True,game
+        return True
 
-def playGame(robDrunk=.5,copDrunk=.5,robEscape=False,copChase=True,rob2MoveProb=0,robPlace='random',copPlace='random'):
+def playGame(robDrunk=.5,copDrunk=.5,robEscape=False,copChase=True,rob2Move=False,robPlace='random',copPlace='random'):
+    oneMoveList = [(a,b) for a in range(-1,2) for b in range(-1,2)]
+    oneMoveList.remove((0,0))
+    twoMoveList = [(a,b) for a in range(-2,3) for b in range(-2,3)]
+    twoMoveList.remove((0,0))
+    
     while True:
         game = createGame()
+        game.oneMoveList = oneMoveList
+        game.twoMoveList = twoMoveList
         
         game.m = int(input('Enter number of rows:'))
         game.n = int(input('Enter number of columns:'))
@@ -174,15 +179,15 @@ def playGame(robDrunk=.5,copDrunk=.5,robEscape=False,copChase=True,rob2MoveProb=
         game.matrix = createMatrix(game)
 
         game.robPlace = placeRob(game,robPlace)
-        game = robChar(game,robDrunk,robEscape,rob2MoveProb)
+        robChar(game,robDrunk,robEscape,rob2Move)
         game.copPlace = placeCop(game,copPlace)
-        game = copChar(game,copDrunk,copChase)
+        copChar(game,copDrunk,copChase)
 
         printMatrix(game,iterCount)
         print('Locations:\n','robber:',game.robPlace,'cop:',game.copPlace,'\n'*2)
 
         while True:
-            caught,game = startChase(game)
+            caught = startChase(game)
             iterCount += 1
 
             printMatrix(game,iterCount)
@@ -198,7 +203,10 @@ def playGame(robDrunk=.5,copDrunk=.5,robEscape=False,copChase=True,rob2MoveProb=
         if playAgain == 'n':
             exit()
 
-playGame(robDrunk=.5,copDrunk=.5,robEscape=True,copChase=True,rob2MoveProb=0,robPlace='random',copPlace='random')
-# placeRob and placeCop are tuples representing placement on a 0-indexed, mxn matrix.
-# Chasing has been implemented for the cop.
-# TODO Still need to implement escape, 2-jump move by robber, and fix matrix bugs.
+
+playGame(robDrunk=.5,copDrunk=.5,robEscape=True,copChase=True,rob2Move=False,robPlace='random',copPlace='random')
+
+# robDrunk and copDrunk are the probability that the respective players DON'T move
+# robEscape and copChase are whether the rob avoids and the cop follows. 'False' means random movement for that player.
+# rob2Move being 'True' means that the robber has a chance to move 1 or 2 spaces on the turns where they do move.
+# placeRob and placeCop are not 'random', input tuples representing one or both's placement on a 0-indexed, mxn matrix.
